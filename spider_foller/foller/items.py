db1cac2515a31e4e184f44c03eb5a1df6404b472
Daniel Ramírez <imdzthor@gmail.com>
2018-05-23 13:44:56 +0100

    Foller Scrapy, con errores

---
+++ spider_foller/foller/items.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class FollerItem(scrapy.Item):
+    username = scrapy.Field()
+    tweets = scrapy.Field()
+    followers = scrapy.Field()
+    following = scrapy.Field()
+    followers_ratio = scrapy.Field()
+    topics = scrapy.Field()
+    hashtags = scrapy.Field()
+    #replies_100 = scrapy.Field()
+    #mentions_100 = scrapy.Field()
+    #hashtags_100 = scrapy.Field()
+    #retweets_100 = scrapy.Field()
+    #links_100 = scrapy.Field()
+    #media_100 = scrapy.Field()
+    #linked_domains_100 = scrapy.Field()
+    #twitter_clients_100 = scrapy.Field()
---
+++ spider_foller/foller/spiders/spider_foller.py
@@ -0,0 +1,39 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from foller.items import FollerItem
+
+class FollerSpider(CrawlSpider):
+    name = 'foller'
+    
+    def start_requests(self):
+        allowed_domains = ['www.foller.me']
+        urls = ['https://foller.me/pokemon']
+        
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+
+    def parse(self, response):
+        
+        fl_item = FollerItem()
+        fl_item['username'] = response.xpath('/html/body/div[3]/div/div/div[2]/div/h1/small').extract()
+        fl_item['tweets'] = response.xpath('//*[@id="overview"]/div[2]/div[4]/table/tbody/tr[1]/td[2]').extract()
+        fl_item['followers'] = response.xpath('//*[@id="overview"]/div[2]/div[4]/table/tbody/tr[2]/td[2]').extract()
+        fl_item['following'] = response.xpath('//*[@id="overview"]/div[2]/div[4]/table/tbody/tr[3]/td[2]').extract()
+        fl_item['followers_ratio'] = response.xpath('//*[@id="overview"]/div[2]/div[4]/table/tbody/tr[4]/td[2]').extract()
+        fl_item['topics'] = response.xpath("//p[@id = 'topics-cloud']").extract()
+        fl_item['hashtags'] = response.xpath('//*[@id="topics"]/div[3]/div[2]/p').extract()
+        #fl_item['replies_100'] = response.xpath('(//*[@id="tweets"]/div[2]/div[2]/table/tbody/tr[1]/td[2]').extract()
+        #fl_item['mentions_100'] = response.xpath('(//*[@id="tweets"]/div[2]/div[2]/table/tbody/tr[2]/td[2]').extract()
+        #fl_item['hashtags_100'] = response.xpath('(//*[@id="tweets"]/div[2]/div[2]/table/tbody/tr[3]/td[2]').extract()
+        #fl_item['retweets_100'] = response.xpath('(//*[@id="tweets"]/div[2]/div[2]/table/tbody/tr[4]/td[2]').extract()
+        #fl_item['links_100'] = response.xpath('(//*[@id="tweets"]/div[2]/div[2]/table/tbody/tr[5]/td[2]').extract()
+        #fl_item['media_100'] = response.xpath('(//*[@id="tweets"]/div[2]/div[2]/table/tbody/tr[6]/td[2]').extract()
+        #fl_item['linked_domains_100'] = response.xpath('(//*[@id="tweets"]/div[2]/div[2]/table/tbody/tr[7]/td[2]').extract()
+        #fl_item['twitter_clients_100'] = response.xpath('(//*[@id="tweets"]/div[2]/div[2]/table/tbody/tr[8]/td[2]').extract()
+        
+        yield fl_item
+        
+
+
